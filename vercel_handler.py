db18f73994a1a8c76ffdf24c90ded9104748b8e0
Mohamedmh3 <syo358814@gmail.com>
2025-08-27 01:01:56 +0300

    vercel_handler

--- vercel_handler.py
+++ vercel_handler.py
@@ -1,5 +1,5 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Delni_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delni-backend.settings")
 app = get_asgi_application()
